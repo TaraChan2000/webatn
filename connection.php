e6ccdedb4280b25177f2b43c6aca4ce4a247dcb8
TaraGCC19004 <91651550+TaraGCC19004@users.noreply.github.com>
2021-11-10 22:36:04 +0700

    done

--- connection.php
+++ connection.php
@@ -1,4 +1,7 @@
 <?php
-   $conn = mysqli_connect('localhost', 'root', '', 'webstore')
-           or die("Can not connect database".mysqli_connect_error());
+    $conn = pg_connect("postgres://dcyptriyociycl:bddd02d896698d806b2831bf5ced6eac69e080f71b153621f6ebf521034948bc@ec2-34-192-174-151.compute-1.amazonaws.com:5432/d9eo07kpacoht7");
+    //$conn = pg_connect("postgresql://postgres:kieungan0711@localhost:5432/postgres");
+    if(!$conn){
+        die("Can not connect database");
+    } 
 ?>
