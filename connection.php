291c8bdaec54f985e91d8d1b9ccd7987277abcbb
TaraGCC19004 <91651550+TaraGCC19004@users.noreply.github.com>
2021-11-11 07:49:19 +0700

    done

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
     $conn = pg_connect("postgres://dcyptriyociycl:bddd02d896698d806b2831bf5ced6eac69e080f71b153621f6ebf521034948bc@ec2-34-192-174-151.compute-1.amazonaws.com:5432/d9eo07kpacoht7");
-    //$conn = pg_connect("postgresql://postgres:kieungan0711@localhost:5432/postgres");
+    //$conn = pg_connect("postgresql://postgres:danhtara2000@localhost:5432/postgres");
     if(!$conn){
         die("Can not connect database");
     } 
